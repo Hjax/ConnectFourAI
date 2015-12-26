45f86ff50a7da5fb08edad33982267a60e2646e5
Kevin Konrad <Kevintkonrad@gmail.com>
2015-12-25 22:05:55 -0500

    WORKING THREAT DETECTION?

--- connectfour.py
+++ connectfour.py
@@ -10,9 +10,7 @@ def chunks(l, n):
 
 # runs can be updated whenever a peg is placed
 # threats need to be updated in a 6 by 6 box around every placed peg, this is slow as dirt
-            #       2 3 4
-            #       1 A 5
-            #       0 7 6
+
 
 # self.board[row][column]
 
@@ -21,12 +19,15 @@ class Root_Node:
         self.board = [[0 for x in range(0, 7)] for x in range(0, 6)]
         self.runs = {}
         # how many squares to add to a location to move a certain direction on the grid
-        self.dmap = {0:6, 1:-1, 2:-8, 3:-7, 4:-6, 5:1, 6:8, 7:7}
+        self.dmap = {0:(1, -1), 1:(0, -1), 2:(-1, -1), 3:(-1, 0), 4:(-1, 1), 5:(0, 1), 6:(1, 1), 7:(1, 0)}
+            #       2 3 4
+            #       1 A 5
+            #       0 7 6
         for row in range(0, 6):
             for column in range(0, 7):
                 self.runs[(row, column)] = [0, 0, 0, 0, 0, 0, 0]
         
-        self.threats = [] # list of int threats, sign determines side
+        self.threats = set() # set of int threats, sign determines side
 
         self.side_to_move = 1
 
@@ -38,31 +39,15 @@ class Root_Node:
         mapper = {0:4, 1:5, 2:6, 3:7, 4:0, 5:1, 6:2, 7:3}
         return mapper[direction]
     def update_direction(self, square, direction):
-        current_loc = square
+        path = self.traverse(square, direction)
         while True:
-            current_loc += self.dmap[direction]
-            if square in range(0, 42):
-                if self.board[current_loc] == 0:
-                    self.runs[current_loc][self.opposite(direction)] += 1 * math.copysign(1, self.board[current_loc]) + self.runs[current_loc + self.dmap[self.opposite(direction)]][self.opposite(direction)] 
-                    
-                    if direction == 3:
-                        if self.runs[current_loc][3] == -3:
-                            pass
-                        elif self.runs[current_loc][3] == 3:
-                            pass
-                    else:
-                        if self.runs[current_loc][direction] == -3:
-                            self.threats.append(current_loc * -1)
-                        elif self.runs[current_loc][direction] == 3:
-                            self.threats.append(current_loc * 1)
-                        if self.runs[current_loc][self.opposite(direction)] == 3:
-                            self.threats.append(current_loc * 1)
-                        elif self.runs[current_loc][self.opposite(direction)] == -3:
-                            self.threats.append(current_loc * -1)
-                        if self.runs[current_loc][direction] + self.runs[current_loc][self.opposite(direction)] <= -3 and current_loc * -1 not in self.threats:
-                            self.threats.append(current_loc * -1)
-                        elif self.runs[current_loc][direction] + self.runs[current_loc][self.opposite(direction)] >= 3 and current_loc not in self.threats:
-                            self.threats.append(current_loc * 1)
+            current_loc = path.next()
+            if self.is_valid(current_loc):
+                if self.tuple_to_board_value(current_loc) == 0:
+                    # the following line might need to be just =
+                    self.runs[current_loc][self.opposite(direction)] += self.tuple_to_board_value(square)
+                    self.runs[current_loc][self.opposite(direction)] += self.runs[(current_loc[0] + self.dmap[self.opposite(direction)][0], current_loc[1] + self.dmap[self.opposite(direction)][1])][self.opposite(direction)]
+                    self.update_threats(current_loc, direction)
                     break # we break when we hit an enemy or blank block 
                 if self.board[current_loc] != self.board[square]:
                     break
@@ -73,10 +58,27 @@ class Root_Node:
                 # we went out of bounds of the board
                 break
     # HELPER FUNCTIONS FOR BOARD INTERACTION
+    def tuple_to_board_value(self, t):
+        return self.board[t[0]][t[1]]
     def number_to_board_value(self, n): # given a number (0-41) returns the value of that board location
         return self.board[n / 7][n % 7]
     def board_tuple_to_number(self, t): # given a tuple for a cordinate of the board, return the numeric value
         return t[0] * 7 + t[1]
+    def traverse(self, location, direction): # generator of all the squares in the direction you give, location as a tuple, direction as an int
+        current_loc = location
+        while True:
+            current_loc = (current_loc[0] + self.dmap[direction][0], current_loc[1] + self.dmap[direction][1])
+            yield current_loc
+    def is_valid(self, location):
+        return location[0] * 7 + location[1] < 42 and  location[0] * 7 + location[1] >= 0
+    def update_threats(self, location, direction): # Takes only one direction, automatically checks both direction
+        if abs(self.runs[location][direction]) == 3:
+            self.threats = self.threats.union(set([self.board_tuple_to_number(location) * math.copysign(1, self.runs[location][direction])]))
+        elif abs(self.runs[location][self.opposite(direction)]) == 3:
+            self.threats = self.threats.union(set([self.tuple_to_board_value(location) * math.copysign(1, self.runs[location][self.opposite(direction)])]))
+        elif abs(self.runs[location][direction] + self.runs[location][self.opposite(direction)]) >= 3:
+            self.threats = self.threats.union(set([self.board_tuple_to_number(location) * math.copysign(1, self.runs[location][direction])]))
+        
     
     def remove_old_threats(self):
         for threat in self.threats:
